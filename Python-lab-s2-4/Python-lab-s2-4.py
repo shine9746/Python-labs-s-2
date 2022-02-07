a9bc79b3fbe325ad877a78d0a5f5e30fa9ce94f2
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-07 11:39:06 +0530

    Update Python-lab-s2-4.py

--- Python-lab-s2-4/Python-lab-s2-4.py
+++ Python-lab-s2-4/Python-lab-s2-4.py
@@ -1,6 +1,6 @@
 #print count  of repeated characters
 
-a = input("Enter a string : ")
+a = input("Enter a string : ").casefold()
 b = {}
 for i in a:
     if i in b:
@@ -12,3 +12,4 @@ for i in b:
     if b[i] > 1:
 
         print(i,":",b[i])
+
