48a0a13e738e67294261ab693cab1d6a729b950c
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-07 12:49:44 +0530

    Add files via upload

---
+++ Python-lab-s2-6/Python-lab-s2-6.py
@@ -0,0 +1,9 @@
+color_dict = {'red':'#FF0000',
+          'green':'#008000',
+          'black':'#000000',
+          'white':'#FFFFFF'}
+
+b = dict(sorted(color_dict.items()))
+
+print(type(b))
+print("Sorted elements are :  ",b)
