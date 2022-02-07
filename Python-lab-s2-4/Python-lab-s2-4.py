0fdd867c33c4e77ee378254496dc6e27be6e3b7c
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-07 11:35:11 +0530

    Add files via upload

---
+++ Python-lab-s2-4/Python-lab-s2-4.py
@@ -0,0 +1,14 @@
+#print count  of repeated characters
+
+a = input("Enter a string : ")
+b = {}
+for i in a:
+    if i in b:
+        b[i] += 1
+    else:
+        b[i] = 1
+
+for i in b:
+    if b[i] > 1:
+
+        print(i,":",b[i])
