b8c0319e328b31e312bf61513de0322cd3ef93a2
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-08 11:42:07 +0530

    Update Python-lab-s2-8.py

--- Python-lab-s2-8/Python-lab-s2-8.py
+++ Python-lab-s2-8/Python-lab-s2-8.py
@@ -1,6 +1,7 @@
-a = ("apple","mango","grape","pineapple","banana","cherry","plum")
+a = ("apple","mango","grape","pineapple","banana","cherry","plum","pomegranate","orange")
 
 print("The fourth element from first is ",a[3] + " " +"and the fourth element from last is", a[-4]+".")
+
 
 
 
