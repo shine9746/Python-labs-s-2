4bf1871ca45d2dd01bba25cf0b78665e2b037603
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-09 10:40:15 +0530

    Add files via upload

---
+++ Python-lab-s2-9/Python-lab-s2-9.py
@@ -0,0 +1,9 @@
+#python programme to find the index of a tuple
+
+a = ("apple","mango","grape","pineapple","banana","pomegranate","jackfruit","cherry","plum","strawberry","lemon")
+
+print(a)
+
+b = input("enter a tuple element : ")
+
+print("The index of tuple element ",b,"is",a.index(b))
