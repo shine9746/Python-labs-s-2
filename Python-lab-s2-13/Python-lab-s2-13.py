109ec4d17625718c3520027375fab8327088f15e
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-10 12:51:05 +0530

    Add files via upload

---
+++ Python-lab-s2-13/Python-lab-s2-13.py
@@ -0,0 +1,13 @@
+a = ["a","b","c","d"]
+
+b = []
+
+for i in range(len(a) + 1):
+
+    for x in range(i + 1, len(a) + 1):
+        b.append(a[i:x])
+
+print("Orginal list is",a)
+
+print("Sublists of a are : ")
+print(b)
